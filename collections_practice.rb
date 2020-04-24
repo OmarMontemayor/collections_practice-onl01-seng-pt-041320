c04034a9c2b07c3d6ea69ac106678e391d341206
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-24 20:58:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,14 +8,12 @@ def sort_array_desc(array)
     array.sort do |a, b|
         b <=> a
     end
-    return array
 end
 
 def sort_array_char_count(array)
     array.sort do |a, b|
         a.length <=> b.length
     end
-    return array
 end
 
 def swap_elements(array)
